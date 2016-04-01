5b6a4c7c6291df5b9e3e44438d885b95472d5853
Vikas Soni <vsoni@aquimo.com>
2016-04-02 00:05:57 +0530

    Authentication token added

--- server.js
+++ server.js
@@ -85,7 +85,14 @@ app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
     
     db.user.authenticate(body).then(function (user) {
-        res.header({'Auth': user.generateToken('authentication')}).json(user.toPublicJSON());
+        var token = user.generateToken('authentication');
+        
+        if(token){
+            res.header({'Auth': user.generateToken('authentication')}).json(user.toPublicJSON());
+        }else{
+            res.status(401).send();
+        }
+        
     }, function (e) {
         res.status(401).send();
     })
